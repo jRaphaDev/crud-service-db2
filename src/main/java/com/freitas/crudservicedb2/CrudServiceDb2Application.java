1c5a44aa6b756718a8c591f96c5aedcc25553d46
Raphael Freitas <dev.rapha@gmail.com>
2018-12-27 21:26:55 -0200

    first commit

---
+++ src/main/java/com/freitas/crudservicedb2/CrudServiceDb2Application.java
@@ -0,0 +1,14 @@
+package com.freitas.crudservicedb2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudServiceDb2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudServiceDb2Application.class, args);
+	}
+
+}
+
